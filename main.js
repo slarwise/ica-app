6c9eb6dc5e1cca94e110cd9bc08c52b1634eeb38
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2023-07-27 23:16:47 +0200

    Add functions that need login
    syncShoppingList does not work

--- main.js
+++ main.js
@@ -29,8 +29,52 @@ async function getRandomRecipe() {
   return recipe.Recipes;
 }
 
+async function getMostVisitedStores() {
+  const url = `${BASE_URL}/api/user/stores`;
+  const result = await fetch(url, { headers });
+  const data = await result.json();
+  return data;
+}
+
+async function getStoreInformation(storeId) {
+  const url = encodeURI(`${BASE_URL}/api/stores/${storeId}`);
+  const result = await fetch(url, { headers });
+  return await result.json();
+}
+
+async function getAllShoppingLists() {
+  const url = `${BASE_URL}/api/user/offlineshoppinglists`;
+  const result = await fetch(url, { headers });
+  return (await result.json()).ShoppingLists;
+}
+
+async function getShoppingList(offlineId) {
+  const url = `${BASE_URL}/api/user/offlineshoppinglists/${offlineId}`;
+  const result = await fetch(url, { headers });
+  return await result.json();
+}
+
+async function syncShoppingList(offlineId, shoppingList) {
+  const url = `${BASE_URL}/api/user/offlineshoppinglists/${offlineId}/sync`;
+  const result = await fetch(url, {
+    headers,
+    method: "POST",
+    body: JSON.stringify(shoppingList),
+  });
+  const data = await result.json();
+  console.log(result.status);
+  return data;
+}
+
 async function main() {
   await login(config.USERNAME, config.PASSWORD);
+  const shoppingLists = await getAllShoppingLists();
+  console.log(shoppingLists);
+  console.log(shoppingLists[0].Rows);
+  const offlineId = shoppingLists[0].OfflineId;
+  const shoppingList = await getShoppingList(offlineId);
+  const newShoppingList = await syncShoppingList(offlineId, shoppingList);
+  console.log(newShoppingList);
 }
 
 main();
