701c089f8396cf85a8b28aa6e241c9bea39c71cf
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2023-07-27 23:14:50 +0200

    Add login step

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 const BASE_URL = "https://handla.api.ica.se";
 
+let headers = {};
+
+const config = {
+  USERNAME: process.env.PERSONAL_NUMBER,
+  PASSWORD: process.env.ICA_PASSWORD,
+};
+
+async function login(username, password) {
+  const url = encodeURI(`${BASE_URL}/api/login`);
+  const base64Credentials = Buffer.from(
+    `${username}:${password}`,
+    "utf8",
+  ).toString("base64");
+  const result = await fetch(url, {
+    headers: {
+      Authorization: `Basic ${base64Credentials}`,
+    },
+  });
+  const authenticationTicket = result.headers.get("AuthenticationTicket");
+  headers.AuthenticationTicket = authenticationTicket;
+}
+
 async function getRandomRecipe() {
   const url = encodeURI(`${BASE_URL}/api/recipes/random?numberofrecipes=1`);
   const result = await fetch(url);
@@ -8,8 +30,7 @@ async function getRandomRecipe() {
 }
 
 async function main() {
-  const recipe = await getRandomRecipe();
-  console.log(recipe);
+  await login(config.USERNAME, config.PASSWORD);
 }
 
 main();
